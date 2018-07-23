553a340a404caab0c97869c20814742374418361
nikola <nikola@daotech.ru>
2018-07-24 00:23:09 +0400

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/home/nikola/projects/opencartquest/admin/');
+define('DIR_SYSTEM', '/home/nikola/projects/opencartquest/system/');
+define('DIR_IMAGE', '/home/nikola/projects/opencartquest/image/');
+define('DIR_LANGUAGE', '/home/nikola/projects/opencartquest/admin/language/');
+define('DIR_TEMPLATE', '/home/nikola/projects/opencartquest/admin/view/template/');
+define('DIR_CONFIG', '/home/nikola/projects/opencartquest/system/config/');
+define('DIR_CACHE', '/home/nikola/projects/opencartquest/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/nikola/projects/opencartquest/system/storage/download/');
+define('DIR_LOGS', '/home/nikola/projects/opencartquest/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/nikola/projects/opencartquest/system/storage/modification/');
+define('DIR_UPLOAD', '/home/nikola/projects/opencartquest/system/storage/upload/');
+define('DIR_CATALOG', '/home/nikola/projects/opencartquest/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'newuser');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/home/nikola/projects/opencartquest/catalog/');
+define('DIR_SYSTEM', '/home/nikola/projects/opencartquest/system/');
+define('DIR_IMAGE', '/home/nikola/projects/opencartquest/image/');
+define('DIR_LANGUAGE', '/home/nikola/projects/opencartquest/catalog/language/');
+define('DIR_TEMPLATE', '/home/nikola/projects/opencartquest/catalog/view/theme/');
+define('DIR_CONFIG', '/home/nikola/projects/opencartquest/system/config/');
+define('DIR_CACHE', '/home/nikola/projects/opencartquest/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/nikola/projects/opencartquest/system/storage/download/');
+define('DIR_LOGS', '/home/nikola/projects/opencartquest/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/nikola/projects/opencartquest/system/storage/modification/');
+define('DIR_UPLOAD', '/home/nikola/projects/opencartquest/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'newuser');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
